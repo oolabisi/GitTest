34d4122d17b91bee4fcc024c81b56c3671e09748
oolabisi <oolabisiogundairo@gmail.com>
2019-05-14 16:09:27 +0100

    Book.java

--- Book.java
+++ Book.java
@@ -4,4 +4,17 @@ public class Book {
 	private String genre;
 	private int pageNumber;
 	private boolean cover;
+	
+	public void setAuthor(String author) {
+		this.author = author;
 	}
+	public void getAuthor() {
+		return author;
+	}
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+	public void getCover() {
+		return cover;
+	}
+    }
